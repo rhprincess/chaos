c7109a7a6b30c7f5ebe815c79df07b6a3710f0bf
coxylicacid <coxylicacid@gmail.com>
2020-03-26 16:12:43 +0800

    Support for fragment label. Optimize ChaosId, it provide by ChaosProvider Add some docs. now you can use Fragment in ChaosUI

--- chaos/src/main/java/io/rhprincess/chaos/support/fragment/FragmentLabel.kt
+++ chaos/src/main/java/io/rhprincess/chaos/support/fragment/FragmentLabel.kt
@@ -20,14 +20,26 @@ class FragmentLabel {
     var height: Int = -1
 }
 
-inline fun <reified T : Fragment> FrameLayout.fragment(
+class FragmentLabel2 {
+    var fragmentId: ChaosIdName = ""
+        set(value) {
+            field = value
+            if (!ChaosId.isExists(value)) {
+                ChaosId.bind(value)
+            }
+        }
+    var width: Int = -1
+    var height: Int = -1
+}
+
+fun FrameLayout.fragment(
     supportFragmentManager: FragmentManager,
     init: FragmentLabel.() -> Unit
-): T {
+): Fragment {
     val wrapper = FragmentLabel()
     wrapper.init()
     this.id = wrapper.fragmentId.toChaosId
-    val fragment = Class.forName(wrapper.name).newInstance() as T
+    val fragment = Class.forName(wrapper.name).newInstance() as Fragment
     supportFragmentManager.beginTransaction().add(wrapper.fragmentId.toChaosId, fragment).commit()
     fragment.view?.layoutParams = FrameLayout.LayoutParams(
         wrapper.width,
@@ -38,7 +50,18 @@ inline fun <reified T : Fragment> FrameLayout.fragment(
 
 inline fun <reified T : Fragment> FrameLayout.fragment(
     supportFragmentManager: FragmentManager,
-    fId: ChaosIdName
+    init: FragmentLabel2.() -> Unit,
+    fId: String = ""
 ): T {
-    return fragment(supportFragmentManager) { fragmentId = fId }
+    val wrapper = FragmentLabel2()
+    wrapper.fragmentId = fId
+    wrapper.init()
+    this.id = wrapper.fragmentId.toChaosId
+    val fragment = T::class.java.getDeclaredConstructor().newInstance()
+    supportFragmentManager.beginTransaction().add(wrapper.fragmentId.toChaosId, fragment).commit()
+    fragment.view?.layoutParams = FrameLayout.LayoutParams(
+        wrapper.width,
+        wrapper.height
+    )
+    return fragment
 }
